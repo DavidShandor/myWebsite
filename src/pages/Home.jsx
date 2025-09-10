5ecaa82d719ba816fb6a83249592467562dd1474
דוד שנדור <davidshandor15@gmail.com>
2025-09-10 19:14:00 +0300

    contact, project and toast. need to polish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,34 @@
-import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
-import { ProjectsSection } from "../components/ProjectSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+// import { Footer } from "../components/Footer";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-        {/* Theme Toggle */}
-        <ThemeToggle />
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
 
-        {/* Background Effects */}
-        
-        <StarBackground />
-        
-        {/* Navbar */}
-        <Navbar />
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
 
-
-        {/* Main Content */}
-        <main>
-            <HeroSection />
-            <AboutSection />
-            <SkillsSection />
-            <ProjectsSection />
-        </main>
-        
-        {/* Footer */}
-
+      {/* Footer */}
+      {/* <Footer /> */}
     </div>
-}
+  );
+};
