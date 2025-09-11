a09a65e3c560802720138c2fa7c3eaf9bfceb18d
דוד שנדור <davidshandor15@gmail.com>
2025-09-11 18:01:15 +0300

    projects images and from submitting with ajax

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -45,7 +45,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./src/assets/David Shandor CV.pdf"
+                href="./public/David Shandor CV.pdf"
                 target="_ blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
